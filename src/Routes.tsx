1b7a4c924419c254995c9a0c4539f67a65b77e15
Ming233 <mingz6@outlook.com>
2021-03-29 02:12:30 -0600

    UI and Navigation

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,26 @@
-import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './HomePage';
-import CovidCases from './CovidCases';
-import TreatmentsAndVaccines from './TreatmentsAndVaccines';
+import * as React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./HomePage";
+import CovidCases from "./CovidCases";
+import TreatmentsAndVaccines from "./TreatmentsAndVaccines";
+import HealthInfo from "./HealthInfo";
 
 class Routes extends React.Component<any, {}> {
-	public render() {
-		return (
-			<React.Fragment>
-				<Switch>
-					<Route path="/home" component={HomePage} />
-					<Route path="/covid-cases" component={CovidCases} />
-					<Route path="/treatments-and-vaccines" component={TreatmentsAndVaccines} />
-                </Switch>
-            </React.Fragment>);
-    }
+  public render() {
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route path="/covid-cases" component={CovidCases} />
+          <Route
+            path="/treatments-and-vaccines"
+            component={TreatmentsAndVaccines}
+          />
+        </Switch>
+        <Route path="/health-info" component={HealthInfo} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Routes;
